bdd5824e8273bb23e96111b010f91ca50d512a65
Jasper Van der Jeugt <jaspervdj@gmail.com>
2011-05-14 11:42:12 +0200

    Convert CaseMap benchmark to use criterion

--- tests/benchmarks/CaseMap.hs
+++ tests/benchmarks/CaseMap.hs
@@ -1,21 +1,24 @@
-import Control.Exception
-import Control.Monad
-import Data.Time.Clock
-import Data.ByteString as B
-import Data.Text.Encoding as T
-import Data.Text as T
-import System.Environment
+-- | This benchmark converts a number of UTF-8 encoded files to uppercase.
+-- It takes the list of files on stdin, one filename on each line. It then uses
+-- the criterion library to run a the benchmark on each file & get reliable
+-- results.
+--
+import Control.Exception (evaluate)
+import Control.Applicative ((<$>))
+import qualified Data.ByteString as B
+import qualified Data.Text.Encoding as T
+import qualified Data.Text as T
 
-time act = do
-  start <- getCurrentTime
-  act
-  end <- getCurrentTime
-  let d = diffUTCTime end start
-  print d
+import Criterion.Main
 
+main :: IO ()
 main = do
-  args <- getArgs
-  forM_ args $ \f -> do
-      t <- T.decodeUtf8 `fmap` B.readFile f
-      evaluate t
-      time $ evaluate (T.toUpper t)
+    files <- lines <$> getContents
+    defaultMain =<< mapM bench' files
+  where
+    -- Read & evaluate the entire file, then convert it to upper case in a pure
+    -- benchmark.
+    bench' file = do
+        t <- T.decodeUtf8 `fmap` B.readFile file
+        _ <- evaluate t
+        return $ bench ("CaseMap " ++ file) $ nf T.toUpper t
