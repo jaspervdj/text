9f81863ecaaea367b7b3052a5f3feeea8da8abe5
Jasper Van der Jeugt <jaspervdj@gmail.com>
2011-06-29 09:34:59 +0200

    Use bytes in cbits for UTF-8

--- cbits/cbits.c
+++ cbits/cbits.c
@@ -3,11 +3,11 @@
 void _hs_text_memcpy(void *dest, size_t doff, const void *src, size_t soff,
 		     size_t n)
 {
-  memcpy(dest + (doff<<1), src + (soff<<1), n<<1);
+  memcpy(dest + doff, src + soff, n);
 }
 
 int _hs_text_memcmp(const void *a, size_t aoff, const void *b, size_t boff,
 		    size_t n)
 {
-  return memcmp(a + (aoff<<1), b + (boff<<1), n<<1);
+  return memcmp(a + aoff, b + boff, n);
 }
